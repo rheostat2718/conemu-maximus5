a38a647ecf0b4c6cd39256a8a4b0397255a912f9
ConEmu.Maximus5 <ConEmu.Maximus5@90859970-1e37-fbe9-063f-f7b1e201b3eb>
2012-02-19 23:18:02 +0000

    Resolve plugin

---
+++ trunk/Resolve/version.h
@@ -0,0 +1,29 @@
+
+#define MVV_1 0
+#define MVV_2 9
+#define MVV_3 0
+#define MVV_4 FARMANAGERVERSION_BUILD
+#define MVV_4a ""
+
+#ifdef _UNICODE
+	#if FAR_UNICODE>=1906
+		#include "common/far3/pluginW3.hpp"
+	#else
+		#include "common/unicode/pluginW.hpp"
+	#endif
+#else
+	#include "common/ascii/pluginA.hpp"
+#endif
+
+
+#define STRING2(x) #x
+#define STRING(x) STRING2(x)
+
+#define MYDLLVERS STRING(MVV_1) "." STRING(MVV_2) "." STRING(MVV_3) "#" STRING(MVV_4) "\0"
+#define MYDLLVERN MVV_1,MVV_2,MVV_3,MVV_4
+
+#ifdef _WIN64
+#define MYDLLPLTFRM " (x64)"
+#else
+#define MYDLLPLTFRM " (x86)"
+#endif
