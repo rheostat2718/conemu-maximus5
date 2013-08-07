44e603ac38c48b83f166f5e50143fb4a1af6d7a3
ConEmu.Maximus5 <ConEmu.Maximus5@90859970-1e37-fbe9-063f-f7b1e201b3eb>
2013-08-07 23:17:48 +0000

    ConEmu Preview

--- trunk/ConEmu-preview/src/Setup/Setupper/VersionI.h
+++ trunk/ConEmu-preview/src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,7,3,0
-#define CONEMUVERS "130703"
-#define CONEMUVERL L"130703"
-#define MSI86 "../ConEmu.130703.x86.msi"
-#define MSI64 "../ConEmu.130703.x64.msi"
+#define CONEMUVERN 13,8,7,0
+#define CONEMUVERS "130807"
+#define CONEMUVERL L"130807"
+#define MSI86 "../ConEmu.130807.x86.msi"
+#define MSI64 "../ConEmu.130807.x64.msi"
