abed85b9611c668cb6a9e24742ba4c5ebcf35103
ConEmu.Maximus5 <ConEmu.Maximus5@90859970-1e37-fbe9-063f-f7b1e201b3eb>
2014-03-04 20:26:58 +0000

    ConEmu 140304 Alpha sources

--- ConEmu-alpha/src/Setup/Setupper/VersionI.h
+++ ConEmu-alpha/src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,3,2,0
-#define CONEMUVERS "140302"
-#define CONEMUVERL L"140302"
-#define MSI86 "../ConEmu.140302.x86.msi"
-#define MSI64 "../ConEmu.140302.x64.msi"
+#define CONEMUVERN 14,3,4,0
+#define CONEMUVERS "140304"
+#define CONEMUVERL L"140304"
+#define MSI86 "../ConEmu.140304.x86.msi"
+#define MSI64 "../ConEmu.140304.x64.msi"
