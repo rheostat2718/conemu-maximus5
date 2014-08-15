ec15d27a4e2f9015daf4e981ff233abfc3fde3ab
ConEmu.Maximus5 <ConEmu.Maximus5@90859970-1e37-fbe9-063f-f7b1e201b3eb>
2014-08-15 07:59:01 +0000

    ConEmu 140814 Alpha sources

--- ConEmu-alpha/src/ConEmu/ConEmuCtrl.cpp
+++ ConEmu-alpha/src/ConEmu/ConEmuCtrl.cpp
@@ -1185,7 +1185,9 @@ void CConEmuCtrl::ChooseTabFromMenu(BOOL abFirstTabOnly, POINT pt, DWORD Align /
 			CRealConsole* pRCon = pVCon->RCon();
 			if (pRCon)
 			{
-				pRCon->ActivateFarWindow(nNewR);
+				CTab tab(__FILE__,__LINE__);
+				if (pRCon->GetTab(nNewR, tab))
+					pRCon->ActivateFarWindow(tab->Info.nFarWindowID);
 			}
 			if (!gpConEmu->isActive(pVCon))
 				gpConEmu->Activate(pVCon);
@@ -1210,8 +1212,6 @@ bool CConEmuCtrl::key_ShowTabsList(DWORD VkMod, bool TestOnly, const ConEmuHotKe
 		if (TestOnly)
 			return true;
 
-		//HMENU hPopup = gpConEmu->CreateVConListPopupMenu(NULL, FALSE);
-
 		RECT rcWnd = {};
 		if (pRCon)
 		{
@@ -1226,62 +1226,7 @@ bool CConEmuCtrl::key_ShowTabsList(DWORD VkMod, bool TestOnly, const ConEmuHotKe
 		POINT pt = {(rcWnd.left+rcWnd.right)/2, (rcWnd.top+rcWnd.bottom)/2};
 
 		ChooseTabFromMenu(FALSE, pt, TPM_CENTERALIGN|TPM_VCENTERALIGN);
-
-		//int nTab = gpConEmu->trackPopupMenu(tmp_TabsList, hPopup, TPM_CENTERALIGN|TPM_VCENTERALIGN|TPM_RETURNCMD,
-		//	x, y, 0, ghWnd, NULL);
-		//if (nTab >= IDM_VCON_FIRST && nTab <= IDM_VCON_LAST)
-		//{
-		//	int nNewV = ((int)HIWORD(nTab))-1;
-		//	int nNewR = ((int)LOWORD(nTab))-1;
-		//	
-		//	CVirtualConsole* pVCon = gpConEmu->GetVCon(nNewV);
-		//	if (pVCon)
-		//	{
-		//		CRealConsole* pRCon = pVCon->RCon();
-		//		if (pRCon)
-		//		{
-		//			pRCon->ActivateFarWindow(nNewR);
-		//		}
-		//		if (!gpConEmu->isActive(pVCon))
-		//			gpConEmu->Activate(pVCon);
-		//	}
-		//}
-
-		//CESERVER_REQ_GETALLTABS::TabInfo* pTabs = NULL;
-		//int Count = (int)CConEmuCtrl::GetOpenedTabs(pTabs);
-		//if ((Count < 1) || !pTabs)
-		//{
-		//	SafeFree(pTabs);
-		//	return false;
-		//}
-
-		//WARNING("должна быть подобная функция для DropDown у тулбара");
-		//HMENU hPopup = CreatePopupMenu();
-
-		//int nLastConsole = 0;
-		//for (int i = 0, k = 0; i < Count; i++, k++)
-		//{
-		//	if (nLastConsole != pTabs[i].ConsoleIdx)
-		//	{
-		//		AppendMenu(hPopup, MIF_SEPARATOR, 0, NULL);
-		//		nLastConsole = pTabs[i].ConsoleIdx;
-		//	}
-		//	DWORD nFlags = 0
-		//		| (pTabs[i].Disabled ? (MF_GRAYED|MF_DISABLED) : MF_ENABLED)
-		//		| 
-		//	pItems[k].Selected = (pOut->GetAllTabs.Tabs[i].ActiveConsole && pOut->GetAllTabs.Tabs[i].ActiveTab);
-		//	pItems[k].Checked = pOut->GetAllTabs.Tabs[i].ActiveTab;
-		//	pItems[k].Disabled = pOut->GetAllTabs.Tabs[i].Disabled;
-		//	pItems[k].MsgText = pOut->GetAllTabs.Tabs[i].Title;
-		//	pItems[k].UserData = i;
-		//}
-		//DestroyMenu(hPopup);
 	}
-
-	//int AllCount = Count + pTabs[Count-1].ConsoleIdx;
-	//if (pItems)
-	//{
-	//}
 
 	return true;
 }
@@ -1524,8 +1469,10 @@ size_t CConEmuCtrl::GetOpenedTabs(CESERVER_REQ_GETALLTABS::TabInfo*& pTabs)
 
 		CTab tab(__FILE__,__LINE__);
 		wchar_t szMark[6];
-		for (int T = 0; pRCon->GetTab(T, tab); T++)
+		for (int t = 0; pRCon->GetTab(t, tab); t++)
 		{
+			int T = tab->Info.nFarWindowID;
+
 			if (cchCount >= cchMax)
 			{
 				pTabs = (CESERVER_REQ_GETALLTABS::TabInfo*)realloc(pTabs, (cchMax+32)*sizeof(*pTabs));
@@ -1556,19 +1503,10 @@ size_t CConEmuCtrl::GetOpenedTabs(CESERVER_REQ_GETALLTABS::TabInfo*& pTabs)
 					wcscpy_c(szMark, L"   ");
 			}
 				
-			if (V == nActiveCon)
-			{
-				if (T <= 9)
-					_wsprintf(pTabs[cchCount].Title, SKIPLEN(countof(pTabs[cchCount].Title)) L"[%i/&%i]%s", V+1, T, szMark);
-				//else if (T == 9)
-				//	_wsprintf(pTabs[cchCount].Title, SKIPLEN(countof(pTabs[cchCount].Title)) L"[%i/1&0]%s", V+1, szMark);
-				else
-					_wsprintf(pTabs[cchCount].Title, SKIPLEN(countof(pTabs[cchCount].Title)) L"[%i/%i]%s", V+1, T, szMark);
-			}
+			if ((V == nActiveCon) && (T <= 9))
+				_wsprintf(pTabs[cchCount].Title, SKIPLEN(countof(pTabs[cchCount].Title)) L"[%i/&%i]%s", V+1, T, szMark);
 			else
-			{
 				_wsprintf(pTabs[cchCount].Title, SKIPLEN(countof(pTabs[cchCount].Title)) L"[%i/%i]%s", V+1, T, szMark);
-			}
 
 			int nCurLen = lstrlen(pTabs[cchCount].Title);
 			lstrcpyn(pTabs[cchCount].Title+nCurLen, pRCon->GetTabTitle(tab), countof(pTabs[cchCount].Title)-nCurLen);
